ae014d02367f7eb7cb9715bf66cee356d44f3b56
Brooklyn Merrick <Brooklyn.p.merrick@gmail.com>
2026-09-15 21:46:15 -0600

    pages

--- LMSapp.Web/Program.cs
+++ LMSapp.Web/Program.cs
@@ -16,7 +16,11 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseAntiforgery();
 
